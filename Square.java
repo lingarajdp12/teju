860a96eec617caeeba0eb4b40b84981ac7c07be0
lingarajdp12 <lingarajachcholi1999@gmail.com>
2023-02-11 14:21:02 +0530

    push

---
+++ Square.java
@@ -0,0 +1,15 @@
+class Square
+{
+static void area()
+{
+final int a=5;
+   int s=a*a;
+   System.out.println(s);
+}
+public static void main(String[] args)
+{
+System.out.println("------Main starts-------");
+      area();
+      System.out.println("----------Main ends-------");
+}
+}
